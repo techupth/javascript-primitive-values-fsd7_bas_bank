e972af24b7800c426104c32e26db876cfb76f85f
Arisa Teandum <basarisa.1302@gmail.com>
2024-09-30 15:21:49 +0700

    test: javascript primitive values assignment

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,9 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+
+let productPrice = 27000;
+let productDiscount = 0.25;
+let totalPrice = productPrice - productPrice * productDiscount;
+
+console.log(totalPrice);
